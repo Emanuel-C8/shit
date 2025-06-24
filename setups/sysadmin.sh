b6490cb850553a5f93dbe64ecb0afa6b9c1ca13e
Emanuel-C8 <109445602+Emanuel-C8@users.noreply.github.com>
2025-06-24 12:40:11 +0400

    Update sysadmin.sh

--- setups/sysadmin.sh
+++ setups/sysadmin.sh
@@ -1,6 +1,3 @@
 sudo pacman -S tlp htop nnn tmux tree --noconfirm
 cp config/dotfiles/tmux.conf ~/.tmux.conf
 cp config/pacman.conf /etc
-cp config/unikeyboard config/us /usr/share/X11/xkb/symbols
-cp config/00-keyboard.conf /etc/X11/xorg.conf.d/00-keyboard.conf
-
